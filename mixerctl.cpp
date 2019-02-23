2eae2e5797490f70b21d568eff8c58dc4b57c692
Goran Mekić <meka@tilda.center>
2019-02-23 01:51:42 +0100

    Make it big endian

--- mixerctl.cpp
+++ mixerctl.cpp
@@ -10,44 +10,47 @@
 
 int main()
 {
- int sockfd;
- sockfd = socket(AF_INET, SOCK_DGRAM, 0);
- struct sockaddr_in serv;
+  int sockfd;
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  struct sockaddr_in serv;
 
- serv.sin_family = AF_INET;
- serv.sin_port = htons(10024);
- serv.sin_addr.s_addr = inet_addr("192.168.5.80");
+  serv.sin_family = AF_INET;
+  serv.sin_port = htons(10024);
+  serv.sin_addr.s_addr = inet_addr("192.168.5.80");
 
- socklen_t m = sizeof(serv);
- std::string message = "/lr/mix/on  ,i     ";
- message += (char)1;
+  socklen_t m = sizeof(serv);
+  std::string message = "/ch/01/mix/fader    ,f  ";
+  float value = 0.75;
 
- // std::string message = "/info";
- auto size = message.size();
- std::cout << message << std::endl;
+  std::cout << message << std::endl;
+  for (int i = 3; i >= 0; --i)
+  {
+    message += ((char *)&value)[i];
+  }
+  auto size = message.size();
+  std::transform(message.begin(), message.end(), message.begin(), [](char ch) {
+    return ch == ' ' ? '\0' : ch;
+  });
+  sendto(sockfd, message.data(), size, 0, (struct sockaddr *)&serv, m);
 
- std::transform(message.begin(), message.end(), message.begin(), [](char ch) {
-   return ch == ' ' ? '\0' : ch;
- });
- sendto(sockfd, message.data(), size, 0, (struct sockaddr *)&serv, m);
+  char buffer[256];
+  auto n = recvfrom(sockfd, buffer, 256, 0, (struct sockaddr *)&serv, &m);
+  if (n == -1)
+  {
+    perror("recvfrom");
+  }
+  for (int i = 0; i < n; ++i)
+  {
+    if (buffer[i] == '\0')
+    {
+      buffer[i] = ' ';
+    }
+  }
+  if (n < 256)
+  {
+    buffer[n] = '\0';
+  }
+  std::cout << buffer << std::endl;
 
- // char buffer[256];
- // auto n = recvfrom(sockfd, buffer, 256, 0, (struct sockaddr *)&serv, &m);
- // if (n == -1)
- // {
-   // perror("recvfrom");
- // }
- // for (int i = 0; i < n; ++i) {
-   // if (buffer[i] == '\0')
-   // {
-     // buffer[i] = ' ';
-   // }
- // }
- // if (n < 256)
- // {
-   // buffer[n] = '\0';
- // }
- // std::cout << buffer << std::endl;
-
- return 0;
+  return 0;
 }
