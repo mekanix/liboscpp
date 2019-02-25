bea36a107b4d714cec620be7bb18d21b31eef47a
Goran Mekić <meka@tilda.center>
2019-02-25 04:01:42 +0100

    C only strings

--- mixerctl.cpp
+++ mixerctl.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <sys/socket.h>
@@ -8,27 +7,51 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 
+
+const size_t & convert(char *buffer, void *value, const size_t &size)
+{
+  for (int i = 0; i < size; ++i)
+  {
+    buffer[i] = ((char *)value)[size - i - 1];
+  }
+  return size;
+}
+
+
 int main()
 {
   float value = 0.75;
+  int channel = 1;
   int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
   struct sockaddr_in serv;
   socklen_t m = sizeof(serv);
+  char send_buffer[64] = {0};
+  int size = 0;
 
   serv.sin_family = AF_INET;
   serv.sin_port = htons(10024);
   serv.sin_addr.s_addr = inet_addr("192.168.5.80");
 
-  std::string message = "/ch/01/mix/fader    ,f  ";
-  for (int i = 3; i >= 0; --i)
-  {
-    message += ((char *)&value)[i];
-  }
-  auto size = message.size();
-  std::transform(message.begin(), message.end(), message.begin(), [](char ch) {
-    return ch == ' ' ? '\0' : ch;
-  });
-  sendto(sockfd, message.data(), size, 0, (struct sockaddr *)&serv, m);
+  const char *pre = "/ch/";
+  size += strlen(pre);
+  strncpy(send_buffer, pre, size);
+
+  sprintf(send_buffer + size, "%02d", channel);
+  size += 2;
+
+  const char *post = "/mix/fader";
+  int postLen = strlen(post);
+  strncpy(send_buffer + size, post, postLen);
+  size += postLen;
+  size += 4;
+
+  strncpy(send_buffer + size, ",f", 2);
+  size += 4;
+
+  size += convert(send_buffer + size, &value, sizeof(value));
+  printf("%d\n", size);
+
+  sendto(sockfd, send_buffer, size, 0, (struct sockaddr *)&serv, m);
 
   return 0;
 }
